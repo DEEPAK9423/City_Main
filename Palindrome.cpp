4e233c49702a1e691c87fda81b446002577a01ca
DEEPAK9423 <129076166+DEEPAK9423@users.noreply.github.com>
2026-03-31 21:52:16 +0530

    Add palindrome checking function
    Implement a function to check if a string is a palindrome.

---
+++ Palindrome.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+#include<string>
+using namespace std;
+
+bool isPalindrome(string s){
+  int n=s.length();
+  int i=0,j=n-1;
+  while(i<j)
+  {
+    if(s[i++]!=s[j--]) return false;
+    else continue;
+  }
+  return false;
+}
